c03b5b00c171351d896ac390f294470d1a5da786
hlokman <abdallahlokmanhenni@gmail.com>
2023-09-07 20:25:17 +0200

    Edit

---
+++ src/components/contact.js
@@ -0,0 +1,7 @@
+const renderContact = (()=> {
+
+    console.log('test renderContact');
+})();
+
+export {renderContact}
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './style/main/main.css';
-import './style/menu/menu.css'
+import './style/menu/menu.css';
+import './style/contact/contact.css';
+
 import { renderMain } from './components/mainMenu';
 import { renderMenu } from './components/menu';
+import { renderContact } from './components/contact';
 
 //renderMain();
-renderMenu()
+//renderMenu()
 console.log('this is a test')
