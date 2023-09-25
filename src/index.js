698d5f873382f8ef15b15b433ef71d64464ab40f
hlokman <abdallahlokmanhenni@gmail.com>
2023-09-25 21:05:17 +0200

    Edit

--- src/index.js
+++ src/index.js
@@ -24,8 +24,6 @@ content.addEventListener('click', (e) => {
         content.innerHTML='';
         renderContact();
     }
-
-    console.log(e.target.className)
 })
 
-console.log('this is a test')
+
